ada96158cb87ae7f8dfcddde9504836b5ef4307c
Shariq Hashmi <shariq.xiiv@gmail.com>
2025-05-04 17:29:12 +0530

    update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
 
-// Function to populate voices dropdown
+
 function populateVoices() {
   voices = speechSynthesis.getVoices();
   voicesDropdown.innerHTML = voices
@@ -13,13 +13,13 @@ function populateVoices() {
     .join('');
 }
 
-// Set voice based on selection
+
 function setVoice() {
   msg.voice = voices.find(voice => voice.name === this.value);
-  toggle(); // Restart speech with new voice if speaking
+  toggle(); 
 }
 
-// Toggle speaking
+
 function toggle(startOver = true) {
   speechSynthesis.cancel();
   if (startOver && msg.text.trim() !== '') {
@@ -27,12 +27,11 @@ function toggle(startOver = true) {
   }
 }
 
-// Update rate/pitch/text
+
 function setOption() {
   msg[this.name] = this.value;
 }
 
-// Event listeners
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voicesDropdown.addEventListener('change', setVoice);
 options.forEach(option => option.addEventListener('change', setOption));
@@ -40,5 +39,5 @@ options.forEach(option => option.addEventListener('input', setOption));
 speakButton.addEventListener('click', toggle);
 stopButton.addEventListener('click', () => toggle(false));
 
-// Default message text
+
 msg.text = document.querySelector('[name="text"]').value;
